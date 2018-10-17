5b2fc4f1d67d664db1e35860049a3f68d910090f
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2018-10-16 17:07:57 -0700

    :add logic for division

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,13 @@ var multiply = function(number1, number2){
   return number1*number2;
 }
 
+//Division
+var divide = function(number1, number2){
+  return number1/number2;
+}
+
 alert(add(number1, number2));
-alert(subtract(number1,number2));
+alert(subtract(number1, number2));
 alert(multiply(number1, number2));
+alert(divide(number1, number2));
 
