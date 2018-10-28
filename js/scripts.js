d60b474b90aa83642b14565efd5670588c18de0a
Ainur Dzhaianbaeva <ainurdzhaianbaeva@MacBook-Pro-7.local>
2018-10-27 18:58:32 -0700

    add front-end logic for multiplying numbers

--- js/scripts.js
+++ js/scripts.js
@@ -38,4 +38,12 @@ $(document).ready(function(){
     var result = subtract(number1, number2)
     $("#output").text(result);
   })
+
+  $("form#multiply").submit(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#multiply1").val());
+    var number2 = parseInt($("#multiply2").val());
+    var result = multiply(number1, number2)
+    $("#output").text(result);
+  })
 });
